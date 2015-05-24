ffd803ff9420c4b4e05e00de24a71a20d6cd2b1d
Jackie Luo <jacquelineluo95@gmail.com>
2015-05-24 18:53:52 -0500

    Create functional contact form, working on CSRF validation

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_mail import Mail
+from flask.ext.mail import Message
+
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(__name__)
+mail = Mail(app)
+
+
+from config import SECRET_KEY
+from forms import ContactForm
 
 
 @app.route("/")
@@ -20,9 +28,19 @@ def initiatives():
 def team():
     return render_template('team.html')
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subject.data,
+                    recipients=['coreboard@columbia.edu'],
+                    sender=(form.name.data, form.email.data))
+        msg.body = form.message.data
+        mail.send(msg)
+        return redirect(url_for('index'))
+    return render_template('contact.html',
+                            form=form)
+
 
 if __name__ == "__main__":
     app.debug = True
