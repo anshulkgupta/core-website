5a071795474e4fdc43ac536c31ec78f29845c2cb
Jackie Luo <jacquelineluo95@gmail.com>
2015-05-27 15:15:40 -0500

    Fix contact form, now sending emails

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask.ext.mail import Message
 
 
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_object('config')
 mail = Mail(app)
 
 
@@ -12,36 +12,36 @@ from config import SECRET_KEY
 from forms import ContactForm
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route("/calendar")
+@app.route('/calendar')
 def calendar():
     return render_template('calendar.html')
 
-@app.route("/initiatives")
+@app.route('/initiatives')
 def initiatives():
     return render_template('initiatives.html')
 
-@app.route("/team")
+@app.route('/team')
 def team():
     return render_template('team.html')
 
-@app.route("/contact", methods=['GET', 'POST'])
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
         msg = Message(form.subject.data,
-                    recipients=['coreboard@columbia.edu'],
-                    sender=(form.name.data, form.email.data))
-        msg.body = form.message.data
+                    recipients=['coreboard@columbia.edu'])
+        msg.body = """
+        From: %s <%s>
+        %s
+        """ % (form.name.data, form.email.data, form.message.data)
         mail.send(msg)
         return redirect(url_for('index'))
-    return render_template('contact.html',
-                            form=form)
+    return render_template('contact.html', form=form)
 
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug = True, host='0.0.0.0')
