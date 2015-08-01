679ffcb16b3f303b75035a4ce42621fb70ecc04e
Anshul Gupta <anshul.dlf@gmail.com>
2015-08-01 15:15:31 -0400

    team bio, social media layout
    if bio isn’t entered, doesn’t do anything otherwise it renders the bio
    by removing the picture and the title from the card to make space.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 	console.log("Welcome to the CORE website!");
 
-	// Implement scroll spy
+	// Team page manipulation
+	$(".team-card-square").find(".team-bio").hide()
+
+	$(".team-card-square").click(function(e) { 
+	   // bio not yet entered
+	   if($(this).find(".team-bio").html().length < 25)
+	   	return;
+
+       $(this).find(".team-pic").toggle();
+       $(this).find(".team-title").toggle();
+       $(this).find(".team-byline").toggle();
+       $(this).find(".team-bio").toggle();
+    });
+
+    $(".team-socialmedia").click(function(e) {
+    	console.debug(e);
+    	e.stopPropagation();
+    });
 });
